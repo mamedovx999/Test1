65bc8a2e587d35c6f5ff228abd2ecb472a50e1f9
mamedovx999 <mamedovx999@code.edu.az>
2024-02-16 20:03:44 +0400

    Proje dosyası ekle.

---
+++ Test/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+#region
+//Array necedene 5 var ?
+//int[] number = { 5, 15, 43, 23, 56,5,5,5,};
+//int count = 0;
+//for (int i = 0; i < number.Length; i++)
+//{
+//    if (number[i] == 5)
+//    {
+//        count++;
+//    } ;
+//}
+//Console.WriteLine(count);
+#endregion
+#region
